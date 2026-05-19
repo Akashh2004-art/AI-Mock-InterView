a5c6b17f62dcf4f7b2fc43bbfbcb54b8d9f599b6
Akashh2004-art <akashsaha0751@gmail.com>
2026-05-19 15:44:56 +0530

    Fix build errors

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'TS') return
+        warn(warning)
+      }
+    }
+  }
 })
